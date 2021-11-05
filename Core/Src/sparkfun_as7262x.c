96de921394f66f60f4771c8b7654b6fdf083237a
notabelincoln <abejordandx@gmail.com>
2021-11-05 03:13:31 -0400

    Created sparkfun_as7265x.c

---
+++ Core/Src/sparkfun_as7262x.c
@@ -0,0 +1,25 @@
+/*
+  This is a library written for the AMS AS7265x Spectral Triad (Moonlight)
+  SparkFun sells these at its website: www.sparkfun.com
+  Do you like this library? Help support SparkFun. Buy a board!
+  https://www.sparkfun.com/products/15050
+
+  Written by Nathan Seidle & Kevin Kuwata @ SparkFun Electronics, October 25th, 2018
+
+  Modified by Abraham Jordan to work with STM32F303K8
+
+  The Spectral Triad is a three sensor platform to do 18-channel spectroscopy.
+
+  https://github.com/sparkfun/SparkFun_AS7265X_Arduino_Library
+
+  Development environment specifics:
+  Arduino IDE 1.8.5
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
