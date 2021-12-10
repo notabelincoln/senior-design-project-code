116c2be935215d2fe5433887ef0c69f48f6cfcd8
notabelincoln <abejordandx@gmail.com>
2021-12-10 03:35:07 -0500

    Added comments to expalin the functions in the user_math files and made them inline because they are short

--- Core/Inc/user_math.h
+++ Core/Inc/user_math.h
@@ -3,6 +3,11 @@
 // Header file for basic data processing
 #include "user_global.h"
 
+// normalize sample data
 inline float *normalize_sample_array(float *sample, float *calibrate, uint32_t length);
+
+// return the greater between two floats
 inline float float_max(float x1, float x2);
+
+// compute the lesser of two floats
 inline float float_min(float x1, float x2);
---
+++ Core/Src/user_math.c
@@ -0,0 +1,39 @@
+// user_math.c
+// Abraham Jordan
+// C file for basic data processing
+
+// normalize sample data
+inline float *normalize_sample_array(float *sample, float *calibrate)
+{
+	// counter, min value, max value
+	uint32_t i;
+	float min;
+	float max;
+	float ret_array[SENSOR_DATA_LENGTH];
+
+	// gather the minimum and maximum values, store the value delta into ret_array
+	for (i = 0; i < SENSOR_DATA_LENGTH - 1; i++) {
+		min = float_min(calibrate[i] - sample[i], calibrate[i+1] - sample[i+1]);
+		max = float_max(calibrate[i] - sample[i], calibrate[i+1] - sample[i+1]);
+		ret_array[i] = calibrate[i] - sample[i];
+	}
+
+	// normalize the values in delta_array
+	for (i = 0; i < SENSOR_DATA_LENGTH; i++) {
+		ret_array[i] = ret_array[i] / (max - min);
+	}
+
+	return ret_array;
+}
+
+// return the greater between two floats
+inline float float_max(float x1, float x2)
+{
+	return (x1 >= x2) ? x1 : x2;
+}
+
+// compute the lesser of two floats
+inline float float_min(float x1, float x2)
+{
+	return (x1 < x2) ? x1 : x2;
+}
