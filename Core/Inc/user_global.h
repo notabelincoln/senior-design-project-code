ba33830cbe13da65702b8ade0088d328c7dd90c1
notabelincoln <abejordandx@gmail.com>
2021-12-11 00:50:18 -0500

    Added header guard

--- Core/Inc/user_global.h
+++ Core/Inc/user_global.h
@@ -1,3 +1,20 @@
 #ifndef SENSOR_DATA_LENGTH
 #define SENSOR_DATA_LENGTH 18
 #endif
+
+#ifndef USER_GLOBAL_H
+#define USER_GLOBAL_H
+#include "stm32f3xx_hal.h"
+
+// variables are needed outside main.c, so they should be put here
+extern I2C_HandleTypeDef hi2c1;
+extern SPI_HandleTypeDef hspi1;
+extern UART_HandleTypeDef huart2;
+
+extern float sensor_sample_data[SENSOR_DATA_LENGTH];
+extern float sensor_calibration_data[SENSOR_DATA_LENGTH];
+
+extern HAL_StatusTypeDef ret;
+
+
+#endif
