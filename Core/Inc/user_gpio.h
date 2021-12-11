4cddb75a8d08e75f6213935e9a6b140a152202b2
notabelincoln <abejordandx@gmail.com>
2021-12-11 00:51:12 -0500

    Added header guard to user_gpio.h, renamed user_printstr to uart_printf

--- Core/Inc/user_gpio.h
+++ Core/Inc/user_gpio.h
@@ -1,3 +1,5 @@
+#ifndef USER_GPIO_H
+#define USER_GPIO_H
 #include <stdio.h>
 #include <string.h>
 #include <stdarg.h> //for va_list var arg functions
@@ -6,4 +8,5 @@
 #include "user_global.h"
 
 void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin);
-void user_printstr(const char *fmt, ...);
+HAL_StatusTypeDef uart_printf(const char *fmt, ...);
+#endif
