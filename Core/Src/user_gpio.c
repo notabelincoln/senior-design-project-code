fbcbecd5cb3f5e5efbbb20cad2fdd81afc09b816
notabelincoln <abejordandx@gmail.com>
2021-12-12 18:29:36 -0500

    Changed case GPIO_PIN_11 to write a specific file to a uSD card

--- Core/Src/user_gpio.c
+++ Core/Src/user_gpio.c
@@ -61,6 +61,12 @@ void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin)
 	}
 	break;
 	case GPIO_PIN_11: {
+
+		// file names for the sample data, calibration data, and normalized data
+		char *sample_file_name = "samples.csv";
+		char *calibration_file_name = "calibration.csv";
+		char *normal_file_name = "normal.txt";
+
 		//some variables for FatFs
 		FATFS FatFs; 	//Fatfs handle
 		FIL fil; 		//File handle
@@ -76,67 +82,50 @@ void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin)
 		//Read 30 bytes from "test.txt" on the SD card
 		BYTE readBuf[30];
 
-		uart_printf("\r\n~ SD card demo by kiwih ~\r\n\r\n");
+		uart_printf("Attempting to mount uSD...\r\n");
 
-		HAL_Delay(1000); //a short delay is important to let the SD card settle
+		HAL_Delay(500); //a short delay is important to let the SD card settle
 
 		//Open the file system
 		fres = f_mount(&FatFs, "", 1); //1=mount now
 		if (fres != FR_OK) {
 			uart_printf("f_mount error (%i)\r\n", fres);
 			return;
+		} else {
+			uart_printf("uSD mounted\r\n");
 		}
-
-
 
 		fres = f_getfree("", &free_clusters, &getFreeFs);
 		if (fres != FR_OK) {
 			uart_printf("f_getfree error (%i)\r\n", fres);
 			return;
-		}
-
-		//Formula comes from ChaN's documentation
-		total_sectors = (getFreeFs->n_fatent - 2) * getFreeFs->csize;
-		free_sectors = free_clusters * getFreeFs->csize;
-
-		uart_printf("SD card stats:\r\n%10lu KiB total drive space.\r\n%10lu KiB available.\r\n", total_sectors / 2, free_sectors / 2);
-
-		//Now let's try to open file "test.txt"
-		fres = f_open(&fil, "test.txt", FA_READ);
-		if (fres != FR_OK) {
-			uart_printf("f_open error (%i)\r\n");
-			return;
-		}
-		uart_printf("I was able to open 'test.txt' for reading!\r\n");
-
-		//We can either use f_read OR f_gets to get data out of files
-		//f_gets is a wrapper on f_read that does some string formatting for us
-		TCHAR* rres = f_gets((TCHAR*)readBuf, 30, &fil);
-		if(rres != 0) {
-			uart_printf("Read string from 'test.txt' contents: %s\r\n", readBuf);
 		} else {
-			uart_printf("f_gets error (%i)\r\n", fres);
+			//Formula comes from ChaN's documentation
+			total_sectors = (getFreeFs->n_fatent - 2) * getFreeFs->csize;
+			free_sectors = free_clusters * getFreeFs->csize;
 		}
 
-		//Be a tidy kiwi - don't forget to close your file!
-		f_close(&fil);
+		uart_printf("uSD card stats:\r\n%10lu KiB total drive space.\r\n%10lu KiB available.\r\n", total_sectors / 2, free_sectors / 2);
 
 		//Now let's try and write a file "write.txt"
-		fres = f_open(&fil, "write.txt", FA_WRITE | FA_OPEN_ALWAYS);
+		fres = f_open(&fil, sample_file_name, FA_WRITE | FA_OPEN_ALWAYS);
 		if(fres == FR_OK) {
-			uart_printf("I was able to open 'write.txt' for writing\r\n");
+			uart_printf("I was able to open '%s' for writing\r\n", sample_file_name);
 		} else {
 			uart_printf("f_open error (%i)\r\n", fres);
+			return;
 		}
 
-		//Copy in a string
-		strncpy((char*)readBuf, "a new file is made!", 19);
+		f_lseek(&fil, f_size(&fil));
+
+		strncpy((char*)readBuf, "text is added here\n", 19);
 
 		fres = f_write(&fil, readBuf, 19, &bytesWrote);
 		if(fres == FR_OK) {
-			uart_printf("Wrote %i bytes to 'write.txt'!\r\n", bytesWrote);
+			uart_printf("Wrote %i bytes to '%s'!\r\n", bytesWrote, sample_file_name);
 		} else {
 			uart_printf("f_write error (%i)\r\n");
+			return;
 		}
 
 		//Be a tidy kiwi - don't forget to close your file!
@@ -144,28 +133,10 @@ void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin)
 
 		HAL_Delay(100);
 
-		//Now let's try and write a file "write.txt"
-		fres = f_open(&fil, "write.txt", FA_WRITE | FA_OPEN_ALWAYS);
-		if(fres == FR_OK) {
-			uart_printf("I was able to open 'write.txt' for writing\r\n");
-		} else {
-			uart_printf("f_open error (%i)\r\n", fres);
-		}
-
-		f_lseek(&fil, f_size(&fil));
-		strncpy((char*)readBuf, "a new line is made!", 19);
-		fres = f_write(&fil, readBuf, 19, &bytesWrote);
-		if(fres == FR_OK) {
-			uart_printf("Wrote %i bytes to 'write.txt'!\r\n", bytesWrote);
-		} else {
-			uart_printf("f_write error (%i)\r\n");
-		}
-
-		//Be a tidy kiwi - don't forget to close your file!
-		f_close(&fil);
-
 		//We're done, so de-mount the drive
 		f_mount(NULL, "", 0);
+
+		HAL_Delay(200);
 	}
 	}
 
