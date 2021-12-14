9d2fb3c3bcc00205f86d29ae9c0f2ccf08e7a21a
notabelincoln <abejordandx@gmail.com>
2021-12-14 12:48:31 -0500

    Added header guard

--- Core/Inc/user_lcd.h
+++ Core/Inc/user_lcd.h
@@ -3,9 +3,12 @@
  * Header file for code related to the lcd
  * Authors: Emily Currie & Abraham Jordan
  */
+#ifndef USER_LCD_H
+#define USER_LCD_H
 #include "stm32f3xx_hal.h"
 #include "user_global.h"
 void initialize_LCD();
 void display_LCD(float data[18]);
 void set_output(int pin);
 void reset_output(int pin);
+#endif
