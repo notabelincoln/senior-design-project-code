040b59773b03d78d16db6f9ce0c05a47acc247dc
notabelincoln <abejordandx@gmail.com>
2021-12-14 12:36:56 -0500

    Added function prototypes to control the lcd

--- Core/Inc/user_lcd.h
+++ Core/Inc/user_lcd.h
@@ -1,7 +1,11 @@
 /*
  * user_lcd.h
  * Header file for code related to the lcd
- * Author: Abe Jordan
+ * Authors: Emily Currie & Abraham Jordan
  */
 #include "stm32f3xx_hal.h"
 #include "user_global.h"
+void initialize_LCD();
+void display_LCD(float data[18]);
+void set_output(int pin);
+void reset_output(int pin);
