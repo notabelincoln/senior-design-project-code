546915fb8bcba3b0ddb47749e09a97641ee6b47f
notabelincoln <abejordandx@gmail.com>
2021-12-14 03:13:20 -0500

    Moved gpio exti case statements in user_gpio.c to their own functions in user_buttons.c

---
+++ Core/Src/user_buttons.c
@@ -0,0 +1,218 @@
+#include "user_buttons.h"
+
+HAL_StatusTypeDef get_sample_data()
+{
+	// counter and UART string variables
+	uint8_t i;
+
+	// get sample data and store it into the sample array
+	getDataBins(sensor_sample_data, &hi2c1);
+
+	// display the sample over the uart connection
+	ret = uart_printf("Sample Data:\r\n");
+	HAL_Delay(10);
+	// print out each calibrated sample value
+	for (i = 0; i < 18; i++) {
+		ret = uart_printf("%d nm: %09.3f\r\n", 410 + 25 * i,
+				sensor_calibration_data[i] - sensor_sample_data[i]);
+		HAL_Delay(10);
+	}
+
+	normalize_sample(sensor_sample_data, sensor_calibration_data, sensor_sample_normal);
+
+	ret = uart_printf("------------------------\r\n");
+
+	// display the sample over the uart connection
+	ret = uart_printf("Normalized Sample Data:\r\n");
+	HAL_Delay(10);
+	// print out each calibrated sample value
+	for (i = 0; i < 18; i++) {
+		ret = uart_printf("%d nm: %09.3f\r\n", 410 + 25 * i,
+				sensor_sample_normal[i]);
+		HAL_Delay(10);
+	}
+
+	ret = uart_printf("------------------------\r\n");
+}
+
+HAL_StatusTypeDef get_calibration_data()
+{
+	// counter and UART string variables
+	uint8_t i;
+
+	// get calibration data and store it into the calibration array
+	getDataBins(sensor_calibration_data, &hi2c1);
+
+	// display the calibration data over the uart connection
+	ret = uart_printf("Calibration Data:\r\n");
+	HAL_Delay(10);
+	// print out each calibrated bin value
+	for (i = 0; i < 18; i++) {
+		ret = uart_printf("%d nm: %09.3f\r\n",
+				410 + 25 * i, sensor_calibration_data[i]);
+		HAL_Delay(10);
+	}
+	ret = uart_printf("------------------------\r\n");
+}
+
+HAL_StatusTypeDef write_data_to_sd()
+{
+	uint8_t i;
+
+	// file names for the sample data, calibration data, and normalized data
+	char *sample_file_name = "samples.csv";
+	char *calibration_file_name = "calibration.csv";
+	char *normal_file_name = "normal.csv";
+
+	//some variables for FatFs
+	FATFS FatFs; 	//Fatfs handle
+	FIL fil; 		//File handle
+	FRESULT fres; //Result after operations
+
+	//Let's get some statistics from the SD card
+	DWORD free_clusters, free_sectors, total_sectors;
+
+	FATFS* getFreeFs;
+
+	UINT bytesWrote;
+
+	//Read 30 bytes from "test.txt" on the SD card
+	BYTE readBuf[256];
+
+	uart_printf("Attempting to mount uSD...\r\n");
+
+	HAL_Delay(500); //a short delay is important to let the SD card settle
+
+	//Open the file system
+	fres = f_mount(&FatFs, "", 1); //1=mount now
+	if (fres != FR_OK) {
+		uart_printf("f_mount error (%i)\r\n", fres);
+		return;
+	} else {
+		uart_printf("uSD mounted\r\n");
+	}
+
+	fres = f_getfree("", &free_clusters, &getFreeFs);
+	if (fres != FR_OK) {
+		uart_printf("f_getfree error (%i)\r\n", fres);
+		return;
+	} else {
+		//Formula comes from ChaN's documentation
+		total_sectors = (getFreeFs->n_fatent - 2) * getFreeFs->csize;
+		free_sectors = free_clusters * getFreeFs->csize;
+	}
+
+	uart_printf("uSD card stats:\r\n%10lu KiB total drive space.\r\n%10lu KiB available.\r\n", total_sectors / 2, free_sectors / 2);
+
+	// Attempt to append sample data to "sample.txt"
+	fres = f_open(&fil, sample_file_name, FA_WRITE | FA_OPEN_ALWAYS);
+	if(fres == FR_OK) {
+		uart_printf("I was able to open '%s' for writing\r\n", sample_file_name);
+	} else {
+		uart_printf("f_open error (%i)\r\n", fres);
+		return;
+	}
+
+	f_lseek(&fil, f_size(&fil));
+
+	for (i = 0; i < SENSOR_DATA_LENGTH; i++) {
+		sprintf((char *)readBuf, "%09.3f,", sensor_sample_data[i]);
+		fres = f_write(&fil, readBuf, 10, &bytesWrote);
+		if(fres == FR_OK) {
+			uart_printf("Wrote %i bytes to '%s'!\r\n", bytesWrote, sample_file_name);
+		} else {
+			uart_printf("f_write error (%i)\r\n");
+			return;
+		}
+		HAL_Delay(10);
+	}
+
+	fres = f_write(&fil, "\n", 1, &bytesWrote);
+	if(fres == FR_OK) {
+		uart_printf("Wrote %i bytes to '%s'!\r\n", bytesWrote, sample_file_name);
+	} else {
+		uart_printf("f_write error (%i)\r\n");
+		return;
+	}
+
+	f_close(&fil);
+
+	HAL_Delay(100);
+
+	// Attempt to append sample data to "calibration.txt"
+	fres = f_open(&fil, calibration_file_name, FA_WRITE | FA_OPEN_ALWAYS);
+	if(fres == FR_OK) {
+		uart_printf("I was able to open '%s' for writing\r\n", calibration_file_name);
+	} else {
+		uart_printf("f_open error (%i)\r\n", fres);
+		return;
+	}
+
+	f_lseek(&fil, f_size(&fil));
+
+	for (i = 0; i < SENSOR_DATA_LENGTH; i++) {
+		sprintf((char *)readBuf, "%09.3f,", sensor_calibration_data[i]);
+		fres = f_write(&fil, readBuf, 10, &bytesWrote);
+		if(fres == FR_OK) {
+			uart_printf("Wrote %i bytes to '%s'!\r\n", bytesWrote, calibration_file_name);
+		} else {
+			uart_printf("f_write error (%i)\r\n");
+			return;
+		}
+		HAL_Delay(10);
+	}
+
+	fres = f_write(&fil, "\n", 1, &bytesWrote);
+	if(fres == FR_OK) {
+		uart_printf("Wrote %i bytes to '%s'!\r\n", bytesWrote, calibration_file_name);
+	} else {
+		uart_printf("f_write error (%i)\r\n");
+		return;
+	}
+
+	//Be a tidy kiwi - don't forget to close your file!
+	f_close(&fil);
+
+	HAL_Delay(100);
+
+	// Attempt to append sample data to "normal.txt"
+	fres = f_open(&fil, normal_file_name, FA_WRITE | FA_OPEN_ALWAYS);
+	if(fres == FR_OK) {
+		uart_printf("I was able to open '%s' for writing\r\n", normal_file_name);
+	} else {
+		uart_printf("f_open error (%i)\r\n", fres);
+		return;
+	}
+
+	f_lseek(&fil, f_size(&fil));
+
+	for (i = 0; i < SENSOR_DATA_LENGTH; i++) {
+		sprintf((char *)readBuf, "%09.3f,", sensor_sample_normal[i]);
+		fres = f_write(&fil, readBuf, 10, &bytesWrote);
+		if(fres == FR_OK) {
+			uart_printf("Wrote %i bytes to '%s'!\r\n", bytesWrote, normal_file_name);
+			HAL_Delay(10);
+		} else {
+			uart_printf("f_write error (%i)\r\n");
+			return;
+		}
+		HAL_Delay(10);
+	}
+	fres = f_write(&fil, "\n", 1, &bytesWrote);
+	if(fres == FR_OK) {
+		uart_printf("Wrote %i bytes to '%s'!\r\n", bytesWrote, normal_file_name);
+	} else {
+		uart_printf("f_write error (%i)\r\n");
+		return;
+	}
+
+	f_close(&fil);
+
+	HAL_Delay(100);
+
+	//We're done, so de-mount the drive
+	f_mount(NULL, "", 0);
+
+	HAL_Delay(200);
+}
+
