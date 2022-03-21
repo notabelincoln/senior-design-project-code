6072fb872c12a7d04d465bf50bff1e81dbd82fba
notabelincoln <abejordandx@gmail.com>
2022-03-21 03:38:15 -0400

    Committing modified files and removing deleted files

--- FATFS/Target/ffconf.h
+++ FATFS/Target/ffconf.h
@@ -5,21 +5,16 @@
   ******************************************************************************
   * @attention
   *
-  * <h2><center>&copy; Copyright (c) 2021 STMicroelectronics.
-  * All rights reserved.</center></h2>
+  * Copyright (c) 2022 STMicroelectronics.
+  * All rights reserved.
   *
-  * This software component is licensed by ST under Ultimate Liberty license
-  * SLA0044, the "License"; You may not use this file except in compliance with
-  * the License. You may obtain a copy of the License at:
-  *                             www.st.com/SLA0044
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
   *
   ******************************************************************************
   */
 /* USER CODE END Header */
-
-// for some reason the define doesn't work when placed further down
-#define _VOLUMES    1
-/* Number of volumes (logical drives) to be used. */
 
 #ifndef _FFCONF
 #define _FFCONF 32020	/* Revision ID */
@@ -161,6 +156,7 @@
 / Drive/Volume Configurations
 /----------------------------------------------------------------------------*/
 
+#define _VOLUMES    1
 /* Number of volumes (logical drives) to be used. */
 
 /* USER CODE BEGIN Volumes */
