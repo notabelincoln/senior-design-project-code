c43a9c73a3dacda714d352cd77d3bcd4a5903749
notabelincoln <abejordandx@gmail.com>
2022-03-22 20:04:56 -0400

    Moved function prototypes from top of header to bottom, removed function prototypes, added include statements

--- Core/Inc/user_lcd.h
+++ Core/Inc/user_lcd.h
@@ -5,13 +5,13 @@
  */
 #ifndef USER_LCD_H
 #define USER_LCD_H
+
+#include <stdint.h>
+
 #include "stm32f3xx_hal_gpio.h"
 #include "stm32f303x8.h"
-int initialize_LCD();
-int display_LCD(float data[18]);
-int set_output(int pin);
-int reset_output(int pin);
 
+/* Defines for LCD pins */
 #define LCD_RS_PORT GPIOA
 #define LCD_RW_PORT GPIOA
 #define LCD_EN_PORT GPIOA
@@ -25,7 +25,6 @@ int reset_output(int pin);
 #define LCD_DB6_PORT GPIOB
 #define LCD_DB7_PORT GPIOF
 
-
 #define LCD_RS_PIN GPIO_PIN_3
 #define LCD_RW_PIN GPIO_PIN_1
 #define LCD_EN_PIN GPIO_PIN_8
@@ -38,5 +37,11 @@ int reset_output(int pin);
 #define LCD_DB5_PIN GPIO_PIN_0
 #define LCD_DB6_PIN GPIO_PIN_1
 #define LCD_DB7_PIN GPIO_PIN_1
+
+/* Macros */
+
+/* Function prototypes */
+void lcd_init(void);
+void lcd_display(uint8_t data, uint8_t size);
 
 #endif
